fad3c7676e6ab3e171cca6af5789a04dc8c9182b
Karim Cordilia <k.cordilia@hotmail.com>
2025-02-06 10:49:01 +0100

    Initial Commit

---
+++ AppicalAssessmentTests/AppicalAssessmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppicalAssessmentTests.swift
+//  AppicalAssessmentTests
+//
+//  Created by Karim Cordilia on 06/02/2025.
+//
+
+import Testing
+
+struct AppicalAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
